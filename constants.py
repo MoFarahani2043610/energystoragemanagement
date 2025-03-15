6c50e753bc2f5e7a331f7748f3575294f3085d13
= <=>
2025-03-15 17:12:32 +0100

    modification

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
-# Define learning parameters
+# learning parameters
 ALPHA = 0.1  # Learning rate
 GAMMA = 0.9  # Discount factor
 EPSILON = 1.0  # Initial exploration rate
-EPSILON_DECAY = 0.995  # Decay rate for epsilon
-EPSILON_MIN = 0.01 # Minimum value for epsilon
+EPSILON_DECAY = 0.995   # Epsilon decay for more exploration
+EPSILON_MIN = 0.2 # Minimum value for epsilon
+
 # Discretize the state space
-NUM_BINS_ENERGY = 10  # Number of bins for energy in storage
-NUM_BINS_PRICE = 20   # Number of bins for energy price
+NUM_BINS_ENERGY = 100  # Number of bins for energy in storage
+NUM_BINS_PRICE = 200  # Number of bins for energy price
